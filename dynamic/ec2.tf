077b5fa22018eb10e3d8d4b0d8a0b05b36b283cf
Tejaswi Goli <tejaswigoli@gmail.com>
2025-02-14 10:58:37 +0000

    dynamic

---
+++ dynamic/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "this" {
+ ami = "ami-09c813fb71547fc4f"
+ vpc_security_group_ids = [aws_security_group.allow_tls.id]
+ instance_type = "t2.micro"
+ tags = {
+  Name = "terraform-demo"
+  Purpose = "terraform-practice"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+ name = "allow_tls"
+ description = "Allow TLS inbound and outbound traffic"
+
+dynamic "ingress" {
+ for_each = var.ingress_ports
+ content{
+  from_port = ingress.value["from_port"] 
+  to_port = ingress.value["to_port"]
+  protocol = ingress.value["protocol"]
+  cidr_blocks = ingress.value["cidr_blocks"]
+  }
+}
+
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+tags = {
+ Name = "allow_tls"
+ }
+}
